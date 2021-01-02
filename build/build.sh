3952d46637735d19f76f2a0ea544255e03033009
Cass <dev@cass.si>
2021-01-02 18:03:02 +0000

    fix build script

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,5 @@
+# ran from root directory ../build
+
 #local set private variables
 if test -f ".env"; then
 	set -a
@@ -5,6 +7,7 @@ if test -f ".env"; then
 	set +a
 fi
 
+# babel transpiles into src/_transpiled & files put .html in _includes
 node src/_transpiled/generateGraphs.js
 rm -rf src/_transpiled
 
@@ -27,15 +30,15 @@ LOG_COUNT=$(echo $DAYS_CONFIG | jq -r ".total_days")
 HOUR_COUNT=$(echo $DAYS_CONFIG | jq -r ".total_hours")
 
 echo "Day log count: $LOG_COUNT"
-sed -i -e "s/LOG_COUNT/$LOG_COUNT/g" ../_site/index.html
+sed -i -e "s/LOG_COUNT/$LOG_COUNT/g" ./_site/index.html
 
 echo "Total hours count: $HOUR_COUNT"
-sed -i -e "s/HOUR_COUNT/$HOUR_COUNT/g" ../_site/index.html
+sed -i -e "s/HOUR_COUNT/$HOUR_COUNT/g" ./_site/index.html
 
 #revert back incase we're local & overwritten - hacky...
 echo "Reverting _config.yml back to placeholders..."
 sed -i -e "s/sitekey:.*/sitekey: CAPTCHA_SITE_KEY/g" ./_config.yml
 sed -i -e "s/secretkey:.*/secretkey: CAPTCHA_SECRET_KEY/g" ./_config.yml
-rm _config.yml.bak
+rm _config.yml-e
 
 exit 0

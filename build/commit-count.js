4d94f06b188dea2d8ad20a699de8f7184fceba48
Cass <dev@cass.si>
2021-03-14 15:13:20 +0000

    fix versioning

--- build/commit-count.js
+++ build/commit-count.js
@@ -5,7 +5,7 @@ export default async (config, days) => {
   if (fs.existsSync(FILE_LOCATION)) fs.unlinkSync(FILE_LOCATION);
 
   const commitCount = days.reduce((acc, curr) => acc += curr.commit_count, 0);
-  const html = `<p><b>${commitCount}</b> commits made in the last 6 months</p>`;
+  const html = `<b>${commitCount}</b>`;
 
   fs.writeFileSync(FILE_LOCATION, html);
 }
